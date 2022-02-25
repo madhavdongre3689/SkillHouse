6ae852ab9b00de91fa7534d4c159dddbad656201
Madhav Dongre <madhavdongre3689@gmail.com>
2022-02-25 19:26:36 +0530

    EntityFramework

---
+++ StudentRegistrationApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentRegistrationApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StudentRegistrationApp.DbContext;
+namespace StudentRegistrationApp
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Student student = new Student();
+            student.Name = "Madhav";
+            ApplicationDbContext context = new ApplicationDbContext();
+            context.Students.Add(student);
+            Console.Read();
+        }
+    }
+
+}
---
+++ StudentRegistrationApp/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistrationApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public Department Dept { get; set; }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
